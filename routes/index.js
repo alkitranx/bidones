f7a1b5d5b7387441aa83be642a65ddc8a696bb75
hender <hender.chacon.hc88@gmail.com>
2021-06-12 22:23:40 -0300

    cambio de compu

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ app.use(require('./users'));
 app.use(require('./products'));
 app.use(require('./warehouse'));
 app.use(require('./productMovement'));
-app.use(require('./stock'))
+app.use(require('./stock'));
+app.use(require('./login'))
 
 
 
---
+++ routes/login.js
@@ -0,0 +1,13 @@
+const express = require('express'),
+    app = express();
+
+
+app.post('/login' , (req, res) => {
+
+
+    
+})
+
+
+
+module.exports = app;
